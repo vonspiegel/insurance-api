50c6875d284409411e88646e5454716e920a30d6
Pedro BM <p.bonilla.miralles@gmail.com>
2019-10-03 12:41:02 +0200

    data filtered by id

--- dataId.js
+++ dataId.js
@@ -2,24 +2,23 @@
 
 const axios = require('axios');
 
-const dataById = async () => {
+const dataId = async insertId => {
   try {
     const res = await axios.get(
-      'http://www.mocky.io/v2/5808862710000087232b75ac'
+      `http://www.mocky.io/v2/5808862710000087232b75ac`
     );
 
-    const clients = res.data.clients;
-    const clientId = clients.map(client => {
-      return client.id;
+    const clients = res.data.clients.filter(client => {
+      return client.id === insertId;
     });
 
-    return clientId;
+    return clients;
   } catch (error) {
     return error.message;
   }
 };
 
-dataById()
+dataId('2dbaac64-c13b-4d02-a980-e03627dee50d')
   .then(message => {
     console.log(message);
   })
